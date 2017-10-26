4263be1b83f9fe12347c1f3d1c3f5b0106af090f
pujithampalli <poojithampalli@gmail.com>
2017-10-25 23:52:34 -0700

    added database connection

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+//database configuration
+var pg = require('pg');
+    app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.render('pages/db', {results: result.rows} ); }
+      });
+    });
+  });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
